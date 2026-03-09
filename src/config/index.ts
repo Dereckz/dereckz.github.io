18766edcd69a82742aab4b4334115613fe69dc9c
Dereckz <dereckz1304@gmail.com>
2026-03-08 20:26:51 -0600

    Primero proyecto

--- src/config/index.ts
+++ src/config/index.ts
@@ -25,22 +25,25 @@ export const SITE_CONFIG: SiteConfig = {
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Alejandro Múnez",
+    name: "Eduardo Ruiz",
     specialty: "Mobile & Web Developer",
     summary:
-      "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
-    email: "example@email.com",
+   "Ingeniero en Tecnologías de la Información y comunicaciones. Especializado en Sistemas de Escritorio, Aplicaciones Web y Móviles.",
+    email: "ruizperezeduardo1@gmail.com",
   },
   experience: [
     {
-      company: "Zalmart",
-      position: "Lead Android Developer",
-      startDate: "May 2018",
-      endDate: "Sept 2020",
+      company: "Impulsora Mexicana De Productividad Laboral A.C.",
+      position: "Freelancer",
+      // startDate: "May 2018",
+      //endDate: "Sept 2020",
       summary: [
-        "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
-        "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
-        "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
+        "Desarrollo de una plataforma web de capacitación en línea, diseñada para que la empresa Impulsora Mexicana De Productividad Laboral pudiera ofrecer cursos digitales a sus distintos clientes.",
+        "La plataforma permite distribuir contenido formativo en áreas como:",
+        "•	Habilidades digitales",
+        "•	Psicología",
+        "•	Finanzas",
+        "•	Desarrollo personal y profesional",
       ],
     },
     {
